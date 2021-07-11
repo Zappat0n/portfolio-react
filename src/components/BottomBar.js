ab50c35503e0fab691943d54ec8fcd7c894c4fdc
Angel Barros <albarros1973@gmail.com>
2021-07-11 19:22:46 +0100

    Add BottomBar

---
+++ src/components/BottomBar.js
@@ -0,0 +1,16 @@
+const BottomBar = () => {
+  return (
+    <footer class="mt-auto">
+      <nav class="bottom-navbar d-flex justify-content-around">
+        <ul class="nav justify-content-center align-items-baseline p-0">
+          <li><a href="https://github.com/Zappat0n" target="_blank" rel="noopener noreferrer"><i class="fab fa-github p-3"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/angel-barros/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in p-3"></i></a></li>
+          <li><a href="https://twitter.com/albarros1973" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter p-3"></i></a></li>
+          <li><p class="pl-3">albarros1973@gmail.com</p></li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default BottomBar;
