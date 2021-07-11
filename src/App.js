8161d4c87858aa709b987694c9610a20d4393dd0
Angel Barros <albarros1973@gmail.com>
2021-07-11 09:30:52 +0100

    Initialize

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import NavBar from './components/NavBar'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <span class="circle-1"></span>
+      <span class="circle-2"></span>
+
+    </div>
+  );
+}
+
+export default App;
